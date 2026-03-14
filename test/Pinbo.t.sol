cd01f329626d899bf4b5fc0743e40beb3ab51a25
Anonymous <x@pinbo.eth>
2026-03-14 13:11:15 +0100

    add block number

--- test/Pinbo.t.sol
+++ test/Pinbo.t.sol
@@ -18,6 +18,15 @@ contract PinboTest is Test {
         pinbo.postMessage(message);
     }
 
+    function test_PostMessageUpdatesLatestBlock() public {
+        uint256 initialBlock = pinbo.latestMessageBlock();
+        string memory message = "Update block";
+        pinbo.postMessage(message);
+        uint256 newBlock = pinbo.latestMessageBlock();
+        assertEq(newBlock, block.number);
+        assertGt(newBlock, initialBlock);
+    }
+
     function test_PostMessageEmptyString() public {
         vm.expectRevert("Message cannot be empty");
         pinbo.postMessage("");
@@ -28,7 +37,7 @@ contract PinboTest is Test {
         uint256 gasStart = gasleft();
         pinbo.postMessage(shortMessage);
         uint256 gasUsed = gasStart - gasleft();
-        
+
         // Log gas usage for reference
         console.log("Gas used for short message:", gasUsed);
 
@@ -47,7 +56,7 @@ contract PinboTest is Test {
         uint256 gasStart = gasleft();
         pinbo.postMessage(longMessage);
         uint256 gasUsed = gasStart - gasleft();
-        
+
         console.log("Gas used for 500-byte message:", gasUsed);
         // Long strings cost more gas but should still succeed
         assertLt(gasUsed, 500000);
@@ -56,10 +65,14 @@ contract PinboTest is Test {
     function testFuzz_PostMessage(string memory message) public {
         // Skip empty strings in fuzzing
         vm.assume(bytes(message).length > 0);
-        
+
         vm.expectEmit(true, false, false, true);
         emit MessagePosted(address(this), message, block.timestamp);
         pinbo.postMessage(message);
+    }
+
+    function test_InitialLatestMessageBlockIsZero() public {
+        assertEq(pinbo.latestMessageBlock(), 0);
     }
 }
 
